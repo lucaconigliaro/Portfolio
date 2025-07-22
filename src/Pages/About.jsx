d20f5aa3d4eb483fa6c188153f50b6e1361aad8e
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-07-22 16:06:49 +0200

    feat: add About base structure and sample text

---
+++ src/Pages/About.jsx
@@ -0,0 +1,31 @@
+export default function About() {
+
+    return (
+        <>
+            <div>
+                <h1>Curiosi di sapere qualcosa su di me? Ecco qua.</h1>
+                <p>Mi chiamo Luca Conigliaro e sono un frontend developer con una grande passione per il mondo del web e del design funzionale. Dopo un percorso formativo intensivo e tanti progetti personali, oggi mi sto specializzando nello sviluppo di interfacce moderne usando React, TypeScript e Tailwind CSS.
+                    Mi entusiasma lavorare al confine tra codice e design, trasformare un’idea in un’interfaccia intuitiva e reattiva, curata nei dettagli e ottimizzata per l’esperienza utente. Scrivere codice pulito, leggibile e scalabile è per me una priorità.
+
+                    Dopo aver deciso di rimettermi in gioco professionalmente, ho intrapreso un percorso mirato per diventare sviluppatore web. Ora che ho consolidato le basi del frontend e sto ampliando le mie competenze anche nel backend con PHP, Laravel e l’uso di API REST, sono pronto a mettermi alla prova in un contesto lavorativo reale.
+
+                    Mi piace pensare in modo pratico e orientato alla soluzione, e sono il tipo di persona che non si ferma finché non ha capito come funziona davvero qualcosa.
+
+                    Quando non sono immerso tra componenti, hook o commit, mi trovi spesso ad esplorare nuovi progetti open source, leggere documentazione tecnica o imparare qualcosa di nuovo su Udemy o YouTube.</p>
+            </div>
+
+            <div>
+                <p>Qualche dettaglio veloce su di me </p>
+            </div>
+            <ul>
+                <li>29 anni</li>
+                <li>Background tecnico con formazione fullstack</li>
+                <li>Studente continuo di PHP, Laravel e backend</li>
+                <li>Alla ricerca della mia prima esperienza lavorativa come developer</li>
+                <li>	Aperto a tirocini, stage, apprendistato o collaborazioni remote
+                </li>
+            </ul>
+            <p>Se vuoi darmi fiducia, scrivimi!</p>
+        </>
+    )
+}
