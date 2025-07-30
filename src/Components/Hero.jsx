8908d3854e820b63c383781f522204175ae80557
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-07-30 19:55:26 +0200

    feat: add react-toastify for success notifications and update CV download link

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,26 +1,45 @@
 import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Assicurati di importare lo stile
 
 export default function Hero() {
+    const notify = () => {
+        toast.success("CV scaricato con successo! Grazie per l'interesse 😁", {
+            position: "bottom-right",
+            autoClose: 3000,
+            theme: "dark"
+        });
+    };
+
     return (
         <>
             {/* Hero section */}
             <section className="presentation-section">
-
                 <div className="presentation-card">
                     <h1>Ciao, sono Luca</h1>
                     <p>Dopo un bootcamp fullstack intensivo, mi sono specializzato nel frontend con React. Unisco precisione tecnica e creatività, e sto ampliando le mie competenze sia lato frontend che backend, per crescere come sviluppatore completo.</p>
 
                     <div className="presentation-links">
-                        <button className="btn-cv">Download CV</button>
-                        <Link to="/projects" className="btn-projects">Progetti</Link>
-                    </div>
+                        <a
+                            href="/resume/CV-Luca.pdf"
+                            download="LucaConigliaro_CV"
+                            className="btn-cv"
+                            onClick={notify}
+                        >
+                            Download CV
+                        </a>
 
+                        <Link to="/projects" className="btn-projects">
+                            Progetti
+                        </Link>
+                    </div>
                 </div>
 
                 <div className="presentation-img">
                     <img src="/photos/profile.JPG" alt="" />
                 </div>
 
+                <ToastContainer />
             </section>
         </>
     );
