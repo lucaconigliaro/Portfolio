240169b9494161d068ffdaa12e80ef24fb190311
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-07-31 14:13:08 +0200

    feat: tech stack section with icons

--- src/Components/StackSection.jsx
+++ src/Components/StackSection.jsx
@@ -1,51 +1,105 @@
 export default function StackSection() {
     return (
-        <section className="tech-section full-width-breakout">
+        <section className="full-width-breakout">
             <div className="tech-container">
                 <div className="tech-row">
+
+                    {/* Frontend Design */}
                     <div className="tech-col">
-                        <h5>Front-end Engineer Design</h5>
+                        <h5>Frontend Design</h5>
                         <ul>
-                            <li>React</li>
-                            <li>Vite</li>
-                            <li>Figma</li>
-                            <li>Jest</li>
-                            <li>Bootstrap</li>
-                            <li>Tailwind</li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/React.png" alt="React" /></div>
+                                <span>React</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Vite.svg" alt="Vite" /></div>
+                                <span>Vite</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Figma.png" alt="Figma" /></div>
+                                <span>Figma</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Jest.png" alt="Jest" /></div>
+                                <span>Jest</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Bootstrap.png" alt="Bootstrap" /></div>
+                                <span>Bootstrap</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Tailwind.png" alt="Tailwind" /></div>
+                                <span>Tailwind</span>
+                            </li>
                         </ul>
                     </div>
 
+                    {/* Linguaggi */}
                     <div className="tech-col">
-                        <h5>Languages</h5>
+                        <h5>Linguaggi</h5>
                         <ul>
-                            <li>JavaScript</li>
-                            <li>TypeScript</li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/JavaScript.white.png" alt="JavaScript" /></div>
+                                <span>JavaScript</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/TypeScript.png" alt="TypeScript" /></div>
+                                <span>TypeScript</span>
+                            </li>
                         </ul>
                     </div>
 
+                    {/* DevOps */}
                     <div className="tech-col">
                         <h5>DevOps</h5>
                         <ul>
-                            <li>GitHub</li>
-                            <li>Vercel</li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/GitHub.png" alt="GitHub" /></div>
+                                <span>GitHub</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Vercel.png" alt="Vercel" /></div>
+                                <span>Vercel</span>
+                            </li>
                         </ul>
                     </div>
 
+                    {/* Backend */}
                     <div className="tech-col">
                         <h5>Back-end</h5>
                         <ul>
-                            <li>Node.js</li>
-                            <li>Express.js</li>
-                            <li>Postman</li>
-                            <li>MySql2</li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/node.png" alt="Node.js" /></div>
+                                <span>Node.js</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Express.webp" alt="Express.js" /></div>
+                                <span>Express.js</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Postman.webp" alt="Postman" /></div>
+                                <span>Postman</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/MySql2.png" alt="MySQL" /></div>
+                                <span>MySQL</span>
+                            </li>
                         </ul>
                     </div>
 
+                    {/* Next Step */}
                     <div className="tech-col">
                         <h5>Next Step</h5>
                         <ul>
-                            <li>PHP</li>
-                            <li>Laravel</li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/php.png" alt="PHP" /></div>
+                                <span>PHP</span>
+                            </li>
+                            <li className="img-lang">
+                                <div className="logo-wrapper"><img src="src/assets/logos/Laravel.png" alt="Laravel" /></div>
+                                <span>Laravel</span>
+                            </li>
                         </ul>
                     </div>
                 </div>
